000eb02d12b6ea9b663f001daab0043c8afbf5f4
Kalwe Caramalac <kalwe123@hotmail.com>
2016-05-12 04:37:43 +0000

    foo

--- .zsh/zsh_hooks.zsh
+++ .zsh/zsh_hooks.zsh
@@ -3,11 +3,10 @@
 # hookers
 
 function precmd {
-  # vcs_info
-  # Put the string "hostname::/full/directory/path" in the title bar:
+  # put the string "hostname::/full/directory/path" in the title bar
   echo -ne "\e]2;$PWD\a"
 
-  # Put the parentdir/currentdir in the tab
+  # put the parentdir/currentdir in the tab
   echo -ne "\e]1;$PWD:h:t/$PWD:t\a"
 }
 
