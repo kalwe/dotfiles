884207d9aeb307696c454c624e9fa0df5d9ea3cc
Kalwe Caramalac <kalwe123@hotmail.com>
2016-05-12 05:38:30 +0000

    add func clear cache

--- scripts/daemon/mem.sh
+++ scripts/daemon/mem.sh
@@ -1,4 +1,7 @@
 #!/bin/zsh
 # print in terminal ram used and free in MB
 
+# clear cache
+#sysctl -w vm.drop_caches=3
+
 echo "RAM: used/free "`free -m | tr -s ' ' | sed '/^Mem/!d' | cut -d" " -f 3,4`
